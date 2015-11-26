d508f7d474f0317e4348c9169b54da4cb70f520d
Samarth Dhingra <sd2900@columbia.edu>
2015-11-25 19:31:10 -0500

    Modification to moving closer

--- g9/Player.java
+++ g9/Player.java
@@ -30,7 +30,8 @@ public class Player implements wtr.sim.Player {
 		for (int friend_id : friend_ids)
 			W[friend_id] = 50;
 	}
-
+	
+	
 	private int pick_player(Point[] players, int[] chat_ids) {
 		// Pick the closest not-chatting player with positive or unknown W
 		// value by index in the array
@@ -44,14 +45,17 @@ public class Player implements wtr.sim.Player {
 			if (i == self_idx) continue; // Self
 			if (chat_ids[i] == -1) continue; // Chatting to someone else
 			if (W[players[i].id] == 0) continue; // No value
+			
+			double dd = getDistance(players[i], players[self_idx]);
 
-			if (getDistance(players[i], players[self_idx]) < dist) {
-				dist  = getDistance(players[i], players[self_idx]);
+			if (dd < dist && dd>=0.25 && dd<=4.0) {
+				dist  = dd ;
 				index = i;
 			}
 		}
 		return index;
 	}
+
 	
 	// play function
 	public Point play(Point[] players, int[] chat_ids,
@@ -91,8 +95,24 @@ public class Player implements wtr.sim.Player {
 			else{
 				// If can move closer, move to min dist
 				if(minDistFromOther > 0.25){
-					// Currently, just moving closer linearly
-					double dx = self.x;
+					double slope = Math.abs(Math.atan((chat.y - self.y)/(chat.x - self.x)));
+					double xOffset = 0.5*Math.cos(slope);
+					double yOffset = 0.5*Math.sin(slope);
+					
+
+					double newX = (self.x <=chat.x) ? (self.x + xOffset) : (self.x + xOffset);
+					double newY = (self.y <=chat.y) ? (self.y + yOffset) : (self.y + yOffset);
+					
+					// Move to a new position 0.5 metres away from the current position
+					Point newSelf = new Point(newX, newY, self_id);
+					
+					double dx = (chat.x - newSelf.x)*0.50;
+					double dy = (chat.y - newSelf.y)*0.50;
+					
+					next_id = chat.id;
+					return new Point(dx, dy, self_id);	
+					
+					/*double dx = self.x;
 					double dy = self.y;
 					int factorX = self.x < chat.x ? 1 : -1;
 					int factorY = self.y < chat.y ? 1 : -1;
@@ -134,7 +154,7 @@ public class Player implements wtr.sim.Player {
 					}
 					// Eventually, try to move in a circle around 
 					next_id = chat.id;
-					return new Point(dx, dy, self_id);					
+					return new Point(dx, dy, self_id);	*/			
 				}
 			}
 		}
@@ -159,7 +179,15 @@ public class Player implements wtr.sim.Player {
 		
 		// try to initiate chat if previously not chatting
 		if (i == j)
-			for (Point p : players) {
+		{
+			int ind = pick_player(players, chat_ids);
+			if (ind != -1)
+			{
+				return new Point(0.0, 0.0, players[ind].id);
+			}
+		}
+			
+			/*for (Point p : players) {
 				// skip if no more wisdom to gain
 				if (W[p.id] == 0) continue;
 				// compute squared distance
@@ -169,7 +197,7 @@ public class Player implements wtr.sim.Player {
 				// start chatting if in range
 				if (dd >= 0.25 && dd <= 4.0)
 					return new Point(0.0, 0.0, p.id);
-			}
+			}*/
 		// return a random move
 		double dir = random.nextDouble() * 2 * Math.PI;
 		double dx = 6 * Math.cos(dir);
